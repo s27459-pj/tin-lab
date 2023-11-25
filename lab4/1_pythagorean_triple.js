4cf8f80d2a0093919d224136fd75b314906419b7
Stefan Karczewski <s27459@pjwstk.edu.pl>
2023-11-25 17:29:25 +0100

    Fix pythagorean triple condition

--- lab4/1_pythagorean_triple.js
+++ lab4/1_pythagorean_triple.js
@@ -1,8 +1,15 @@
 function pythagoreanTriple(a, b, c) {
-  return a * a + b * b === c * c;
+  return (
+    a * a + b * b === c * c ||
+    a * a + c * c === b * b ||
+    b * b + c * c === a * a
+  );
 }
 
 console.log(pythagoreanTriple(3, 4, 5)); // true
+console.log(pythagoreanTriple(3, 5, 4)); // true
+console.log(pythagoreanTriple(5, 4, 3)); // true
+
 console.log(pythagoreanTriple(3, 5, 7)); // false
 console.log(pythagoreanTriple(12, 5, 13)); // true
 console.log(pythagoreanTriple(12, 5, 14)); // false
